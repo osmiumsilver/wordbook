82680d741669808700bb1ce5d1f968e1097cb4b0
Fuzzbear <freshisgreat@outlook.com>
2021-12-02 21:22:17 +0800

    Revert "optimization"
    This reverts commit b454c99785a1b8122d56c1297702a895d5b291b0.

--- src/wordbook/data/UpdateWord.java
+++ src/wordbook/data/UpdateWord.java
@@ -4,21 +4,22 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class UpdateWord extends ConnectDatabase {
-    int status;
+    int isOK;
 
     public int updateWord(Word word) {
         connectDatabase();
         try {
             String SQL = "update word_table set meaning = ? where word = ? ";
             PreparedStatement sta = con.prepareStatement(SQL);
-            sta.setString(1, word.getMeaning()); //第1个通配符?的值是word.getMeaning():
-            sta.setString(2, word.getEnglishWord()); //第2个通配符?的值是word.getEnglishWord():
-            status = sta.executeUpdate(); //execute
+            //从左向右数第1个通配符?的值是word.getMeaning():
+            sta.setString(1, word.getMeaning());
+            //从左向右数第2个通配符?的值是word.getEnglishWord():
+            sta.setString(2, word.getEnglishWord());
+            isOK = sta.executeUpdate();
             con.close();
         } catch (SQLException e) {
-            status = 0;
-            e.printStackTrace();
+            isOK = 0;
         }
-        return status;
+        return isOK;
     }
 }
---
+++ src/wordbook/view/HandleDelWord.java
@@ -0,0 +1,29 @@
+package wordbook.view;
+
+import wordbook.data.DelWord;
+import wordbook.data.Word;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HandleDelWord implements ActionListener {
+    DelWordView view;
+
+    public void actionPerformed(ActionEvent e) {
+        String englishWord = view.inputWord.getText();
+        if (englishWord.length() == 0)
+            return;
+        Word word = new Word();
+        DelWord del = new DelWord();
+        word.setEnglishWord(englishWord);
+        int isOK = del.delWord(word);
+        if (isOK != 0)
+            view.hintMess.setText("删除单词成功");
+        else
+            view.hintMess.setText("删除失败，单词不在表里");
+    }
+
+    public void setView(DelWordView view) {
+        this.view = view;
+    }
+}
