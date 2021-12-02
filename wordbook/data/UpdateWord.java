4f2c3c3182fa3b47f1a8d0a7304de7063feee3ee
osmiumsilver <169557899+osmiumsilver@users.noreply.github.com>
2021-12-02 14:42:15 +0800

    Package changed

---
+++ wordbook/data/UpdateWord.java
@@ -0,0 +1,25 @@
+package wordbook.data;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UpdateWord extends ConnectDatabase {
+    int isOK;
+
+    public int updateWord(Word word) {
+        connectDatabase();
+        try {
+            String SQL = "update word_table set meaning = ? where word = ? ";
+            PreparedStatement sta = con.prepareStatement(SQL);
+            //从左向右数第1个通配符?的值是word.getMeaning():
+            sta.setString(1, word.getMeaning());
+            //从左向右数第2个通配符?的值是word.getEnglishWord():
+            sta.setString(2, word.getEnglishWord());
+            isOK = sta.executeUpdate();
+            con.close();
+        } catch (SQLException e) {
+            isOK = 0;
+        }
+        return isOK;
+    }
+}
