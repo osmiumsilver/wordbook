08ad86b182896e3d5288301b71dffec7f27ae305
osmiumsilver <169557899+osmiumsilver@users.noreply.github.com>
2021-12-02 20:24:33 +0800

    optimization

--- src/wordbook/data/Word.java
+++ src/wordbook/data/Word.java
@@ -1,22 +1,26 @@
 package wordbook.data;
+/**
+ * 定义 单词类
+ * get set方法
+ */
 
 public class Word {
     String englishWord;
     String meaning;
 
-    public void setEnglishWord(String englishWord) {
-        this.englishWord = englishWord;
-    }
-
     public String getEnglishWord() {
         return englishWord;
     }
 
-    public void setMeaning(String meaning) {
-        this.meaning = meaning;
+    public void setEnglishWord(String englishWord) {
+        this.englishWord = englishWord;
     }
 
     public String getMeaning() {
         return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
     }
 }
---
+++ src/wordbook/view/UpdateWordHandler.java
@@ -0,0 +1,54 @@
+package wordbook.view;
+
+import wordbook.data.QuerySingleWord;
+import wordbook.data.UpdateWord;
+import wordbook.data.Word;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UpdateWordHandler implements ActionListener {
+    UpdateWordTab view;
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == view.lookWord) {
+            lookWord();
+        } else if (e.getSource() == view.submit) {
+            updateWord();
+        }
+    }
+
+    private void updateWord() {
+        String englishWord = view.inputWord.getText();
+        String meaning = view.inputNewMeaning.getText();
+        if (englishWord.length() == 0 || meaning.length() == 0)
+            return;
+        Word word = new Word();
+        UpdateWord update = new UpdateWord();
+        word.setEnglishWord(englishWord);
+        word.setMeaning(meaning);
+        int isOK = update.updateWord(word);
+        if (isOK != 0)
+            view.hintMess.setText("更新单词成功");
+        else
+            view.hintMess.setText("更新失败，单词不在表里");
+    }
+
+    private void lookWord() {
+        String englishWord = view.inputWord.getText();
+        if (englishWord.length() == 0)
+            return;
+        Word word = new Word();
+        word.setEnglishWord(englishWord);
+        QuerySingleWord query = new QuerySingleWord();
+        Word result = query.queryOneWord(word);
+        if (result != null) {
+            view.inputNewMeaning.setText(result.getMeaning());
+        } else
+            view.hintMess.setText("单词不在表里");
+    }
+
+    public void setView(UpdateWordTab view) {
+        this.view = view;
+    }
+}
