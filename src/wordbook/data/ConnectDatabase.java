6e5617909d41d38fb4ed019f5db3be4311700056
osmiumsilver <169557899+osmiumsilver@users.noreply.github.com>
2021-12-02 20:24:33 +0800

    optimization

--- src/wordbook/data/ConnectDatabase.java
+++ src/wordbook/data/ConnectDatabase.java
@@ -1,22 +1,29 @@
-
-
 package wordbook.data;
+/**
+ * 连接数据库
+ */
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class ConnectDatabase { //连接数据库的类
-    Connection con;
+public class ConnectDatabase {
     public final static String DBDRIVER = "com.mysql.cj.jdbc.Driver";
     public final static String DBURL = "jdbc:mysql://127.0.0.1:3306/wordbook";
     public final static String DBUSER = "root";
     public final static String PASSWORD = "12345678";
+    Connection con;
 
     public final void connectDatabase() {
         try {
-            String uri = "com.mysql.cj.jdbc.driver";
-            con = DriverManager.getConnection(DBURL, DBUSER, PASSWORD); //连接数据库代码
+            Class.forName(DBDRIVER);
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try { //创建名字是MyEnglishBook的数据库
+            con = DriverManager.getConnection(DBURL, DBUSER, PASSWORD); //连接数据库代码
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
