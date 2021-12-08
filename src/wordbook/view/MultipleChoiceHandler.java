ddc93abf9dac7e978030221a73232ab3ac891156
Fuzzbear <freshisgreat@outlook.com>
2021-12-08 10:21:55 +0800

    Edit MultipleChoiceTab

--- src/wordbook/view/MultipleChoiceHandler.java
+++ src/wordbook/view/MultipleChoiceHandler.java
@@ -10,6 +10,7 @@ package wordbook.view;
 import wordbook.data.GetChoices;
 import wordbook.data.Word;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -19,19 +20,51 @@ import java.awt.event.ActionListener;
  */
 public class MultipleChoiceHandler implements ActionListener {
     MultipleChoiceTab view;
-    public void actionPerformed(ActionEvent e){
-        System.out.println(111);
-        GetChoices choices = new GetChoices();
-        Word[] result = choices.getChoices();
-        for(int i=0; i<result.length;i++){
-            System.out.println(result[i].getMeaning());
+    String right_answer;
+    public synchronized void actionPerformed(ActionEvent e){
+//        System.out.println("当前按下的是："+e.getActionCommand());
+
+        String answer = e.getActionCommand();
+
+
+
+        if(answer.equals("开始")){
+            view.bw2.setVisible(true);
+            view.bw3.setVisible(true);
+            view.bw4.setVisible(true);
+            view.hint.setVisible(true);
+        }else{
+            System.out.println("休眠之前");
+            if(answer.equals(right_answer)){
+
+                view.hint.setText("回答正确");
+            }else{
+                view.hint.setText("回答错误，正确答案为："+right_answer);
+            }
+            view.repaint();
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            System.out.println("休眠之后");
         }
 
-        view.question.setText(result[(int)(Math.random()*4+1)].getEnglishWord());
+        GetChoices choices = new GetChoices();
+        Word[] result = choices.getChoices();
+
+        int index = (int)(Math.random()*4);
+
+        // 设置下一题
+        view.question.setText(result[index].getEnglishWord());
         view.bw1.setText(result[0].getMeaning());
         view.bw2.setText(result[1].getMeaning());
         view.bw3.setText(result[2].getMeaning());
         view.bw4.setText(result[3].getMeaning());
+
+
+        right_answer = result[index].getMeaning();
+
     }
 
     public void setView(MultipleChoiceTab view){
