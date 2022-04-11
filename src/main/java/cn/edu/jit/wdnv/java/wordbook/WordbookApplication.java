914237636c86aa5fa0d9c64b120df2d223c7c6c7
osmiumsilver <169557899+osmiumsilver@users.noreply.github.com>
2022-04-11 22:46:26 +0800

    Some basic stuff
    With newly-written unit test

--- src/main/java/cn/edu/jit/wdnv/java/wordbook/WordbookApplication.java
+++ src/main/java/cn/edu/jit/wdnv/java/wordbook/WordbookApplication.java
@@ -1,19 +1,17 @@
 package cn.edu.jit.wdnv.java.wordbook;
-/*
-  Launcher
- */
+// Launcher
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 
 @SpringBootApplication
 @MapperScan("cn.edu.jit.wdnv.java.wordbook.mapper")
-public class Main {
+public class WordbookApplication {
 
     public static void main(String[] args) {
-
-        SpringApplication.run(Main.class, args);//启动窗口
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(WordbookApplication.class);
+        builder.headless(false).run(args); //make it not headless
     }
 }
