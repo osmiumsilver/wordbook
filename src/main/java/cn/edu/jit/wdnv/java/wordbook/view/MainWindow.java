2f397c786ff7d42d369731209dd4d14dfd9d00a9
osmiumsilver <169557899+osmiumsilver@users.noreply.github.com>
2022-04-12 08:20:50 +0800

    Removed useless AllWords Function

--- src/main/java/cn/edu/jit/wdnv/java/wordbook/view/MainWindow.java
+++ src/main/java/cn/edu/jit/wdnv/java/wordbook/view/MainWindow.java
@@ -16,7 +16,7 @@ public class MainWindow extends JFrame {
     AddWordTab addWordView = GetBeanUtil.getBean(AddWordTab.class); //添加单词页面
     DelWordTab delWordView = GetBeanUtil.getBean(DelWordTab.class); //删除单词页面
     GetMeaningTab GetMeaningView = GetBeanUtil.getBean(GetMeaningTab.class); //单个单词页面
-    AllWordsTab allWordsView = GetBeanUtil.getBean(AllWordsTab.class); //所有单词页面
+
     UpdateWordsTab updateWordsView = GetBeanUtil.getBean(UpdateWordsTab.class); //所有单词页
     RandomWordsTab randomWordsView = GetBeanUtil.getBean(RandomWordsTab.class); //随机单词页面
     QuizTab quizView = GetBeanUtil.getBean(QuizTab.class); //
@@ -29,7 +29,6 @@ public class MainWindow extends JFrame {
         tabbedPane.add("添加单词", addWordView);//设置选项卡标题，并附加相应的页
         tabbedPane.add("更新单词", updateWordsView);//设置选项卡标题，并附加相应的页
         tabbedPane.add("删除单词", delWordView);
-        tabbedPane.add("浏览全部单词", allWordsView);
         tabbedPane.add("随机抽取单词", randomWordsView);
         tabbedPane.add("测验", quizView);
         this.add(tabbedPane, BorderLayout.CENTER);
